8e89489082b14e97e262787e1bf9a9b0196fa978
Bridget Kelly <bckelly197@gmail.com>
2022-02-01 13:15:55 -0500

    completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers() {
+    return drivers.slice(0,2);
+}
+
+function returnLastTwoDrivers() {
+    return (drivers.slice(2));
+}
+
+const selectingDrivers = [returnFirstTwoDrivers , returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier){
+    return function(fare) {
+    return multiplier * fare;
+}
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+
+/*function selectDifferentDrivers(arrayOfDrivers, returnFirstTwoDrivers) {
+    return drivers.slice(0,2); 
+}
+function selectDifferentDrivers(arrayOfDrivers, returnLastTwoDrivers) {
+    return drivers.slice(2);
+}
+
+*/
+
+const selectDifferentDrivers = function (arrayOfDrivers, driversToReturn) {
+    return driversToReturn(arrayOfDrivers);
+};
